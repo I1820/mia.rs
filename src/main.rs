2b460552f0f3ba824ad3b4d3b57887bd9131399f
Parham Alvani <parham.alvani@gmail.com>
2022-05-18 08:01:57 +0430

    feat: add sample mqtt usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rumqttc::{Client, MqttOptions, QoS};
+use std::thread;
+use std::time::Duration;
+
+mod mia;
+
 fn main() {
-    println!("Hello, world!");
+    let mut mqttoptions = MqttOptions::new("", "test.mosquitto.org", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    client.subscribe("hello/rumqtt", QoS::AtMostOnce).unwrap();
+    thread::spawn(move || {
+        for i in 0..10 {
+            client
+                .publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize])
+                .unwrap();
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    // Iterate to poll the eventloop for connection progress
+    for (i, notification) in connection.iter().enumerate() {
+        println!("Notification = {:?}", notification);
+    }
 }
